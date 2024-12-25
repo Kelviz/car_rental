a049e62176a47a6d1bf0e7ee52cd209be9690379
root <root@vps6748319a4c035611453602.noezserver.de>
2024-12-25 00:18:22 +0000

    made small changes

--- customers/urls.py
+++ customers/urls.py
@@ -5,8 +5,8 @@ from .views import RegisterView, LoginView, CheckAuthView, TokenRefreshView
 
 
 urlpatterns = [
-    path('auth/register', RegisterView.as_view(), name='register'),
-    path('auth/login', LoginView.as_view(), name='login'),
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', LoginView.as_view(), name='login'),
     path('auth/check-auth/', CheckAuthView.as_view(), name='check-auth'),
     path('auth/refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
